b411bb39e31a440521613e8d591d0ab7c7d3b897
Kavya Goyal <kavyagoel262000@gmail.com>
2024-04-25 11:13:33 +0530

    final commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>© 2024 Edusity. All rights reserved.</p>
+      <ul>
+        <li><a>Terms of Services</a></li>
+        <li><a>Privacy Policy</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Footer
